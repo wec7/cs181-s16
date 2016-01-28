fdac175a7a3e58956528a0ce2d593220ece71ff2
bellmr01 <ryan.bellmore@pfizer.com>
2016-01-28 10:56:21 -0500

    Added assignment one

---
+++ homework1/linreg.py
@@ -0,0 +1,53 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+csv_filename = 'congress-ages.csv'
+times  = []
+ages = []
+
+with open(csv_filename, 'r') as csv_fh:
+
+    # Parse as a CSV file.
+    reader = csv.reader(csv_fh)
+
+    # Skip the header line.
+    next(reader, None)
+
+    # Loop over the file.
+    for row in reader:
+
+        # Store the data.
+        times.append(float(row[0]))
+        ages.append(float(row[1]))
+
+# Turn the data into numpy arrays.
+times  = np.array(times)
+ages = np.array(ages)
+
+# Plot the data.
+plt.plot(times, ages, 'o')
+plt.show()
+
+# Create the simplest basis, with just the time and an offset.
+X = np.vstack((np.ones(times.shape), times)).T
+
+# Nothing fancy for outputs.
+Y = ages
+
+# Find the regression weights using the Moore-Penrose pseudoinverse.
+w = np.linalg.solve(np.dot(X.T, X) , np.dot(X.T, Y))
+
+# Compute the regression line on a grid of inputs.
+# DO NOT CHANGE grid_times!!!!!
+grid_times = np.linspace(75, 120, 200)
+grid_X = np.vstack((np.ones(grid_times.shape), grid_times))
+grid_Yhat  = np.dot(grid_X.T, w)
+
+# Plot the data and the regression line.
+plt.plot(times, ages, 'o',
+        grid_times, grid_Yhat, '-')
+plt.xlabel("Congress age (nth Congress)")
+plt.ylabel("Average age")
+plt.show()
+        
