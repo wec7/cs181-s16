47ce2190c3c78e374c31ed1ccdeed367eb526232
weiyialanchen <weiyi.alan.chen@gmail.com>
2016-02-02 21:41:11 -0500

    practical 1 update

--- practical1/sample.py
+++ practical1/sample.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.dummy import DummyRegressor
-from sklearn.ensemble import AdaBoostRegressor, BaggingRegressor, ExtraTreesRegressor, GradientBoostingRegressor, RandomForestRegressor
-from sklearn.isotonic import IsotonicRegression
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.linear_model import *
-from sknn import mlp
-from sklearn.neighbors import KNeighborsRegressor, RadiusNeighborsRegressor
-from sklearn.cross_decomposition import PLSRegression
-from sklearn.svm import SVR, LinearSVR, NuSVR
-from sklearn.tree import DecisionTreeRegressor, ExtraTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+def write_to_file(filename, predictions):
+    with open(filename, "w") as f:
+        f.write("Id,Prediction\n")
+        for i,p in enumerate(predictions):
+            f.write(str(i+1) + "," + str(p) + "\n")
 
 """
 Read in train and test as Pandas DataFrames
 """
-print "loading data ..."
-df_train = pd.read_csv("train.csv")
-df_test = pd.read_csv("test.csv")
+df_train = pd.read_csv("train_2048.csv")
+df_test = pd.read_csv("test_2048.csv")
 
-print "transofrming data ..."
 #store gap values
 Y_train = df_train.gap.values
 #row where testing examples start
@@ -32,35 +29,18 @@ df_train = df_train.drop(['gap'], axis=1)
 #DataFrame with all train and test examples so we can more easily apply feature engineering on
 df_all = pd.concat((df_train, df_test), axis=0)
 
-print "feature engineering ..."
-"""
-Example Feature Engineering
-
-this calculates the length of each smile string and adds a feature column with those lengths
-Note: this is NOT a good feature and will result in a lower score!
-"""
-#smiles_len = np.vstack(df_all.smiles.astype(str).apply(lambda x: len(x)))
-#df_all['smiles_len'] = pd.DataFrame(smiles_len)
-
 #Drop the 'smiles' column
 df_all = df_all.drop(['smiles'], axis=1)
 vals = df_all.values
 X_train = vals[:test_idx]
 X_test = vals[test_idx:]
+Y_train = Y_train[:test_idx]
+print "Train features:", X_train.shape
+print "Train gap:", Y_train.shape
+print "Test features:", X_test.shape
 
-print "regressor fitting ..."
-# regressor = mlp.Regressor([mlp.Layer("Rectifier")])
-regressor = DummyRegressor()
+regressor = RandomForestRegressor()
 regressor.fit(X_train, Y_train)
 
-print "regressor predicting ..."
 pred = regressor.predict(X_test)
-
-def write_to_file(filename, predictions):
-    with open(filename, "w") as f:
-        f.write("Id,Prediction\n")
-        for i,p in enumerate(predictions):
-            f.write(str(i+1) + "," + str(p) + "\n")
-
-print "writing output ..."
-write_to_file("pred.csv", pred)
+write_to_file("2048RF.csv", pred)
